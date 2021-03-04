f9d30c1384a301a84a6fd4d89a05ddc984748854
Daniel Agustin Tradito <daniel.travito@quisitive.com>
2021-03-04 02:38:28 -0300

    exerc 02 extra04 part02  done

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,23 +3,32 @@
 
 import * as React from 'react'
 
-function useLocalStorageState (key, defaultValue = '') {
+function useLocalStorageState (key, defaultValue = '', {
+  serialize = JSON.stringify,
+  deserialize = JSON.parse
+} = {}) {
  
   const [state, setState] = React.useState(
     () => {
       const valueInLocalStorage = window.localStorage.getItem(key)
       if (valueInLocalStorage) {
-        return JSON.parse(window.localStorage.getItem(key))
+        return deserialize(window.localStorage.getItem(key))
       }
-      return defaultValue
+      //To return a function if defaultValue it's a function
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue
     }
     )
+  const prevKeyRef = React.useRef(key)
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state) )
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if(prevKey !== key) {
+      window.localStorage.setItem(key, serialize(state) )}
+    prevKeyRef.current = key  
+    window.localStorage.setItem(key, serialize(state) )
+  }, [key, state, serialize])
 
-  return [state, setState]
+  return [state, setState, serialize]
 }
 
 function Greeting({initialName = ''}) {
