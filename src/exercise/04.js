368c4c1dd85ff12afd18115fa90d1b5bae1c8baf
Daniel Agustin Tradito <daniel.travito@quisitive.com>
2021-03-20 11:59:01 -0300

    exerc 04 extra01 done

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,9 +4,42 @@
 import * as React from 'react'
 
 function Board() {
+  function useLocalStorageState (
+    key, 
+    defaultValue = '', 
+    {serialize = JSON.stringify,deserialize = JSON.parse} = {},
+    ) {
+   
+    const [state, setState] = React.useState(
+      () => {
+        const valueInLocalStorage = window.localStorage.getItem(key)
+        if (valueInLocalStorage) {
+          try {
+            return deserialize(valueInLocalStorage)
+          } catch(error) {
+            window.localStorage.removeItem(key)
+          } 
+        }
+        //To return a function if defaultValue it's a function
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+      }
+      )
+    const prevKeyRef = React.useRef(key)
+  
+    React.useEffect(() => {
+      const prevKey = prevKeyRef.current
+      if(prevKey !== key) {
+        window.localStorage.removeItem(prevKey)
+      }
+      prevKeyRef.current = key  
+      window.localStorage.setItem(key, serialize(state) )
+    }, [key, state, serialize])
+  
+    return [state, setState]
+  }
   // 🐨 squares is the state for this component. Add useState for squares
   const init_squares = Array(9).fill(null)
-  const [squares, setSquares] = React.useState(init_squares)
+  const [squares, setSquares] = useLocalStorageState('squares', init_squares)
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
