8c646b7d36e9e1bfe7f4669c7540cb359c804a94
Daniel Agustin Tradito <daniel.travito@quisitive.com>
2021-05-04 16:32:59 -0300

    exerc 06 extra03 done

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,40 +8,57 @@ import * as React from 'react'
 // PokemonDataView: the stuff we use to display the pokemon info
 import {PokemonForm, fetchPokemon,  PokemonInfoFallback, PokemonDataView} from '../pokemon'
 
+const initState = {
+  status:'idle',
+  pokemon: null,
+  error: null
+}
+
 function PokemonInfo({pokemonName}) {
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error,  setError] = React.useState(null)
-  const [status, setStatus] = React.useState('idle')
+  const [status, setStatus] = React.useState(initState)
+
+
 
   React.useEffect(() => {
     if(!pokemonName) {
       return
     }
-    setStatus('pending')
+    setStatus(
+      {
+      status:'pending',
+      pokemon: null
+    })
     fetchPokemon(pokemonName).then(
       res => {
-        setPokemon(res)
-        setStatus('resolved')
+        setStatus(
+          {
+          status:'resolved',
+          pokemon: res
+        })
       },
       error => {
-        setError(error)
-        setStatus('rejected')
+        setStatus(
+          {
+          status:'rejected',
+          pokemon: null,
+          error
+        })
       }
     )
   }, [pokemonName])
 
-  if(status==='idle') {
+  if(status.status==='idle') {
     return 'Submit a pokemon'
-  } else if(status === 'pending' ) {
+  } else if(status.status === 'pending' ) {
     return <PokemonInfoFallback name={pokemonName} />
-  } else if(status === 'rejected'){
+  } else if(status.status === 'rejected'){
     return (
       <div role="alert">
-        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{status.error.message}</pre>
       </div>
     )
-  } else if(status==='resolved') {
-    return  <PokemonDataView pokemon={pokemon} />
+  } else if(status.status==='resolved') {
+    return  <PokemonDataView pokemon={status.pokemon} />
   }
   throw new Error('This is impossible!')
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
